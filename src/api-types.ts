55ade34af95abf53068adf112ed734ec6f6ea1b6
Joao Luiz <joaocagnoni@gmail.com>
2026-08-19 15:15:07 -0300

    feat(sdk): add schema_only field to ConnectorResponse type

--- src/api-types.ts
+++ src/api-types.ts
@@ -578,6 +578,8 @@ export interface components {
              * Format: date-time
              */
             created_at: string;
+            /** Schema Only */
+            schema_only?: boolean;
         };
         /** ConnectorSchemaResponse */
         ConnectorSchemaResponse: {
